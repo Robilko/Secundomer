544480388501c75b230af30ee0771d04b3a5de32
Robilko <sasha.robson@gmail.com>
2022-03-01 22:44:14 +0300

    Initial commit

---
+++ app/src/main/java/com/example/secundomer/ElapsedTimeCalculator.kt
@@ -0,0 +1,14 @@
+package com.example.secundomer
+
+class ElapsedTimeCalculator(private val timestampProvider: TimestampProvider) {
+
+    fun calculate(state: StopwatchState.Running): Long {
+        val currentTimestamp = timestampProvider.getMilliseconds()
+        val timePassedSinceStart = if (currentTimestamp > state.startTime) {
+            currentTimestamp - state.startTime
+        } else {
+            0
+        }
+        return timePassedSinceStart + state.elapsedTime
+    }
+}
---
+++ app/src/main/java/com/example/secundomer/TimestampProvider.kt
@@ -0,0 +1,5 @@
+package com.example.secundomer
+
+interface TimestampProvider {
+    fun getMilliseconds() : Long
+}
